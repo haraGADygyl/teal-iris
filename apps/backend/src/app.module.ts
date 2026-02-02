7a2a0875dd5abd97bb8ce6e15f0aee8b0101061a
NewSmoke38 <shivanialive2007@gmail.com>
2026-02-02 22:57:10 +0530

    feat: add and verify husky and lint-staged setup

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { HealthController } from './modules/health/health.controller';
+import { Module } from "@nestjs/common";
+import { HealthController } from "./modules/health/health.controller";
 
 @Module({
   imports: [],
   controllers: [HealthController],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
